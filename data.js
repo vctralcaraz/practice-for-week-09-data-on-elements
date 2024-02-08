c6d9ce097cb3236c4a1a0947fc8fd012697cb7e1
Victor Alcaraz <vctralcaraz@gmail.com>
2024-02-07 23:19:06 -0800

    done

--- data.js
+++ data.js
@@ -1,1 +1,18 @@
 // Your code here
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.getElementById('add');
+    const nameInput = document.getElementById('name');
+    const selectOption = document.getElementById('type');
+    const shoppingList = document.getElementById('shopping-list');
+
+    const handleAdd = e => {
+        e.preventDefault();
+        const li = document.createElement('li');
+        li.innerText = `${nameInput.value} - ${selectOption.value}`;
+        li.setAttribute('data-type', selectOption.value);
+        shoppingList.appendChild(li);
+        nameInput.value = '';
+    }
+
+    addButton.addEventListener('click', handleAdd);
+});
